131a8b9b31d049db1de28ad2c66dfe4b0c956926
Matthew Brooks <mdeanbrooks@gmail.com>
2015-09-21 16:03:55 -0700

    Add time/date formatting for form submissions, use Trucks to make server request, not API request

--- public/AppView.js
+++ public/AppView.js
@@ -6,15 +6,35 @@ var AppView = Backbone.View.extend({
   <input class="submitButton" type="submit"/>'),
 
   events: {
-    'click .formSubmit' : 'formSubmit',
-    'keydown input'   : 'formSubmit'
+    'click .formSubmit': 'formSubmit',
+    'keydown input'    : 'formSubmit'
   },
 
   formSubmit : function(e) {
     if(e.which === 13) {
       console.log('hello');
-      this.collection.getTime();
+      var loc = $('.inputField').val();
+      var time = this.formatTime();
+      var location = this.formatLocation(loc);
+      this.collection.getTrucks(location, time);
     }
+  },
+
+  formatLocation : function (location) {
+    return location.split(' ').join('+') + ',San+Francisco=';
+  },
+
+  formatTime : function () {
+    var daysKey = {
+      "Sun": 0, "Mon": 1, "Tue": 2, "Wed": 3, 
+      "Thur": 4, "Fri": 5, "Sat": 6
+    };
+
+    var now = new Date().toString().split(' ');
+    var time =  {
+      day: daysKey[now[0]],
+      hour: now[4].slice(0,2)
+    };
   },
 
   initialize: function () {
