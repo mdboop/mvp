ae0a703f314829f043d79168118513a2324bc107
Matthew Brooks <mdeanbrooks@gmail.com>
2015-09-21 14:37:29 -0700

    Refactor getTime to format data for API call

--- public/app.js
+++ public/app.js
@@ -1,17 +1,33 @@
 var App = Backbone.Model.extend({
 
-  defaults: {
+  url: 'https://data.sfgov.org/resource/jjew-r69b.json',
 
+  defaults: {
+    dayOrder: {
+      "Sun": 0,
+      "Mon": 1,
+      "Tue": 2,
+      "Wed": 3,
+      "Thur": 4,
+      "Fri": 5,
+      "Sat": 6
+    }
   },
 
-  getFoodTrucks: function(location) {
+  getFoodTrucks: function(location, time) {
 
   },
 
   getTime: function() {
+
+    var key = {
+      "Sun": 0, "Mon": 1, "Tue": 2, "Wed": 3, 
+      "Thur": 4, "Fri": 5, "Sat": 6
+    };
+
     var now = new Date().toString().split(' ');
-    var day = now[0];
-    var time = now[4];
+    var day = key[now[0]];
+    var time = now[4].slice(0,2);
     var result =  {
       day: day,
       hour: time
